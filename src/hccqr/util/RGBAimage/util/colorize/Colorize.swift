988ff8d0d4f5557251a33578dbf2a03acd060b81
Eonist <30n1st@gmail.com>
2019-02-18 22:07:42 +0100

    Fix mac bug regarding mem leak

--- src/hccqr/util/RGBAimage/util/colorize/Colorize.swift
+++ src/hccqr/util/RGBAimage/util/colorize/Colorize.swift
@@ -14,7 +14,7 @@ internal class Colorize {
    internal static func colorize(images:[Image], colorMap:ColorMap, moduleMultiplier:Int, scale:Int) -> Image? {
       let rgbaImages:[RGBAImage] = images.compactMap{RGBAImage.rgbaImage(image: $0)}
       guard images.count == rgbaImages.count else {Swift.print("Colorize.colorize() - some rgbaImages was not created");return nil}
-      guard let result:RGBAImage = colorize(rgbaImages: rgbaImages, colorMap: colorMap, moduleMultiplier:moduleMultiplier,scale:scale) else {Swift.print("Colorize.colorize() - Unable to create colorized rgbaImage");return UIImage()}
+      guard let result:RGBAImage = colorize(rgbaImages: rgbaImages, colorMap: colorMap, moduleMultiplier:moduleMultiplier,scale:scale) else {Swift.print("Colorize.colorize() - Unable to create colorized rgbaImage");return nil}
       guard let image:Image = RGBAImage.image(rgbaImage: result, scale:CGFloat(scale)) else {Swift.print("Colorize.colorize() - Unable to convert to UIImage");return nil}
       result.pixels.deallocate()/*⚠️️⚠️️We get a mem leak in iOS if we dont deallocate the pixels⚠️️⚠️️*/
       return image
