ddccd479d2abb04382bbdcdfe732543c936a1f32
Eonist <30n1st@gmail.com>
2019-12-21 20:03:00 +0100

    Optimize scale method

--- Sources/HCCQR-lib/util/RGBAimage/util/image/RGBAImageScaler.swift
+++ Sources/HCCQR-lib/util/RGBAimage/util/image/RGBAImageScaler.swift
@@ -3,28 +3,34 @@ import CoreImage
 
 class RGBAImageScaler {
    /**
-    * Scales img without becoming blurry (Pixel multiplier)
-    * - Parameter multiplier: The amount to scale the pixel by
-    */
-   static func scale(rgbaImage: RGBAImage, multiplier: Int) -> RGBAImage {
-      let pixels: [PixelData] = Array(rgbaImage.pixels)
-      return RGBAImageScaler.scale(pixels: rgbaImage.pixels, size: rgbaImage.size, multiplier: multiplier)
-   }
-   /**
     * Scales img without becoming blurry (Sharp pixel multiplier)
     * - Note: This method is used when creating HCCQR images from data
+    * - Note: assert if scaling is needed before callign this method
     * - Fixme: ⚠️️ add the concurrent optimization for nested for loops
     * - Parameter multiplier: The amount to scale the pixel by
     */
    static func scale(pixels: UnsafeMutableBufferPointer<PixelData>, size: RGBAImage.Size, multiplier: Int) -> RGBAImage {
       Swift.print("multiplier:  \(multiplier)")
-      // - fixme: ⚠️️ Add a check if multiplier isnt needed
-      let resultPixels: [PixelData] = (0..<size.height * multiplier).flatMap { y in // Arranges the pixel grid
-         (0..<size.width * multiplier).map { x in
+      let multipliedWidth = size.width * multiplier
+      let multipliedHeight = size.height * multiplier
+      let capacity = multipliedWidth * multipliedHeight
+      let resultPixels = UnsafeMutableBufferPointer<PixelData>.allocate(capacity: capacity)//[PixelData]()
+      // - Fixme: ⚠️️ try to do the concurrentPerform on the inner loop
+      DispatchQueue.concurrentPerform(iterations: multipliedHeight) { y in // optimization initiative, might be faster
+         (0..<size.width * multiplier).indices.forEach { x in
             let pixelIndex: Int = y / multiplier * size.height + x / multiplier
-            return pixels[pixelIndex]
+            let index: Int = y * multipliedWidth + x
+            resultPixels[index] = pixels[pixelIndex]
          }
       }
-      return .rgbaImage(pixels: resultPixels, size: (width: size.width * multiplier, height: size.height * multiplier))
+      return .init(pixels: resultPixels, width: multipliedWidth, height: multipliedHeight)
    }
 }
+
+//      let resultPixels: [PixelData] = (0..<size.height * multiplier).flatMap { y in // Arranges the pixel grid
+//         (0..<size.width * multiplier).map { x in
+//            let pixelIndex: Int = y / multiplier * size.height + x / multiplier
+//            return pixels[pixelIndex]
+//         }
+//      }
+//      return .rgbaImage(pixels: resultPixels, size: (width: size.width * multiplier, height: size.height * multiplier))
--- Sources/HCCQR-lib/util/RGBAimage/util/image/RGBAImageUtil.swift
+++ Sources/HCCQR-lib/util/RGBAimage/util/image/RGBAImageUtil.swift
@@ -33,3 +33,51 @@ public final class RGBAImageUtil {
       return cgImage
    }
 }
+
+// Fixme: ⚠️️ you could try the bellow and see if its faster?
+//struct PixelData {
+//   var a: UInt8 = 0
+//   var r: UInt8 = 0
+//   var g: UInt8 = 0
+//   var b: UInt8 = 0
+//}
+//
+//func imageFromBitmap(pixels: [PixelData], width: Int, height: Int) -> UIImage? {
+//   assert(width > 0)
+//   
+//   assert(height > 0)
+//   
+//   let pixelDataSize = MemoryLayout<PixelData>.size
+//   assert(pixelDataSize == 4)
+//   
+//   assert(pixels.count == Int(width * height))
+//   
+//   let data: Data = pixels.withUnsafeBufferPointer {
+//      return Data(buffer: $0)
+//   }
+//   
+//   let cfdata = NSData(data: data) as CFData
+//   let provider: CGDataProvider! = CGDataProvider(data: cfdata)
+//   if provider == nil {
+//      print("CGDataProvider is not supposed to be nil")
+//      return nil
+//   }
+//   let cgimage: CGImage! = CGImage(
+//      width: width,
+//      height: height,
+//      bitsPerComponent: 8,
+//      bitsPerPixel: 32,
+//      bytesPerRow: width * pixelDataSize,
+//      space: CGColorSpaceCreateDeviceRGB(),
+//      bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue),
+//      provider: provider,
+//      decode: nil,
+//      shouldInterpolate: true,
+//      intent: .defaultIntent
+//   )
+//   if cgimage == nil {
+//      print("CGImage is not supposed to be nil")
+//      return nil
+//   }
+//   return UIImage(cgImage: cgimage)
+//}
