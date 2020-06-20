071bbb8ec4f715b4832b3be0b2475210f2b99613
eonist <30n1st@gmail.com>
2020-06-20 13:28:10 +0200

    Pre cleaning up channel

--- Sources/HCCQR-lib/util/RGBAimage/util/Splitter/channel/DEPRECATED/RGBA/Channel+RGBA+Handler.swift
+++ Sources/HCCQR-lib/util/RGBAimage/util/Splitter/channel/DEPRECATED/RGBA/Channel+RGBA+Handler.swift
@@ -10,7 +10,7 @@ extension Channel {
     * - Fixme: ⚠️️ We could Return 3 GrayScaleImages instead of 3 RGBAImages, might be faster
     * - Parameter rgbImg must be dealocated in the completion block because it is consumes 3 times
     */
-   static func onChannelComplete(i: Int, rgbaImage: RGBAImage, rgbaImages: inout [RGBAImage?], rgbaImg: RGBAImage, onComplete: OnChannelsCompleted) {
+   static func onChannelCompleteDEPRECATED(i: Int, rgbaImage: RGBAImage, rgbaImages: inout [RGBAImage?], rgbaImg: RGBAImage, onComplete: OnChannelsCompleted) {
       rgbaImages[i] = rgbaImage // it matters which order the qrImages came in when you stitch them back together
       if !rgbaImages.contains(where: { $0 == nil }) { // makes sure all images finished (fastest way to check for nil)
          let rgbaImages: [RGBAImage] = rgbaImages.compactMap { $0 } // remove optionality
--- Sources/HCCQR-lib/util/RGBAimage/util/Splitter/channel/DEPRECATED/RGBA/Channel+RGBA.swift
+++ Sources/HCCQR-lib/util/RGBAimage/util/Splitter/channel/DEPRECATED/RGBA/Channel+RGBA.swift
@@ -4,7 +4,7 @@ final class Channel {}
 /**
  * Channels
  */
-extension Channel {
+extension Channel {}
    /**
     * ⚠️️⚠️️⚠️️ DEPRECATED ⚠️️⚠️️⚠️️
     * Split 1 RGBAImage into 3 RGBAImages and then into 3 b&w RGBAImages consisting of singular r, g, b channels (⚠️️ white represents the channel color ⚠️️)
@@ -14,9 +14,9 @@ extension Channel {
     *   - channelMap: rule-set for the splitting process
     *   - onComplete: notify when process has completed
     */
-   static func channels(rgbaImg: RGBAImage, channelMap: ChannelMap = channelMap, onComplete:@escaping OnChannelsCompleted) {
+//   static func channelsDEPRECATD(rgbaImg: RGBAImage, channelMap: ChannelMap = channelMap, onComplete:@escaping OnChannelsCompleted) {
       // - Fixme: ⚠️️ Move assertions into const let outside method
-      fatalError("⚠️️⚠️️⚠️️ DEPRECATED ⚠️️⚠️️⚠️️")
+//      fatalError("⚠️️⚠️️⚠️️ DEPRECATED ⚠️️⚠️️⚠️️")
 //      let assertions: [(PixelData) -> Bool] = channelMap.map { rgbColor in { $0.isColorish(rgbColor) } }
 //      var rgbaImages: [RGBAImage?] = [RGBAImage?](repeating: nil, count: assertions.count) // Fixme: ⚠️️ we could use unmanaged pointer with capacity as well, might be faster
 //      assertions.enumerated().forEach { item in // 3 assertions
@@ -27,5 +27,4 @@ extension Channel {
 //            }
 //         }
 //      }
-   }
-}
+//   }
